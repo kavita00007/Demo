552dbc136c0ff7f42abfafb91aabd5fbe40e3220
kavita00007 <kavitarpatil27@gmail.com>
2020-08-03 15:54:01 +0530

    first sample demo java file

---
+++ src/test/java/landingPage/Hotlinks.java
@@ -0,0 +1,12 @@
+package landingPage;
+
+public class Hotlinks {
+
+	public static void main(String[] args) {
+System.out.println("Test1");
+System.out.println("Test2");
+System.out.println("Test3");
+
+	}
+
+}
